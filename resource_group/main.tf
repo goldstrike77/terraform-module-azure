a0e12ef5b1ea875653787703eeaff62314397de4
suzhetao <suzhetao@gmail.com>
2020-10-29 18:12:37 +0800

    modified:   network_security_group/main.tf 	modified:   resource_group/main.tf 	modified:   subnet/main.tf 	modified:   virtual_network/main.tf

--- resource_group/main.tf
+++ resource_group/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = "var.resource_group_name"
-  location = "var.location"
+  name     = var.resource_group_name
+  location = var.location
   tags     = {
-      owner       = "lookup(var.tag, owner)"
-      email       = "lookup(var.tag, email)"
-      title       = "lookup(var.tag, title)"
-      department  = "lookup(var.tag, department)"
-      location    = "lookup(var.tag, location)"
-      project     = "lookup(var.tag, project)"
-      environment = "lookup(var.tag, environment)"
+    owner       = lookup(var.tag, var.tag.owner, "somebody")
+    email       = lookup(var.tag, var.tag.email, "somebody@mail.com")
+    title       = lookup(var.tag, var.tag.title, "Engineer")
+    department  = lookup(var.tag, var.tag.department, "IS")
+    location    = lookup(var.tag, var.tag.location, "chinanorth")
+    project     = lookup(var.tag, var.tag.project, "Test")
+    environment = lookup(var.tag, var.tag.environment, "SIT")
   }
 }
--- virtual_network/main.tf
+++ virtual_network/main.tf
@@ -1,25 +1,24 @@
 module "resource_group" {
   source              = "../resource_group"
-  resource_group_name = "var.resource_group_name"
-  location            = "var.location"
-  tag                 = "var.tag"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tag                 = var.tag
 }
 
 resource "azurerm_virtual_network" "virtual_network" {
-  name                = "var.virtual_network_name"
+  name                = var.virtual_network_name
   depends_on          = [module.resource_group]
-  resource_group_name = "var.resource_group_name"
-  address_space       = ["var.virtual_network_cidr"]
-  dns_servers         = "var.virtual_network_dns"
-  location            = "var.location"
-
-  tags = {
-      owner       = "lookup(var.tag, owner)"
-      email       = "lookup(var.tag, email)"
-      title       = "lookup(var.tag, title)"
-      department  = "lookup(var.tag, department)"
-      location    = "lookup(var.tag, location)"
-      project     = "lookup(var.tag, project)"
-      environment = "lookup(var.tag, environment)"
-    }
+  resource_group_name = var.resource_group_name
+  address_space       = [var.virtual_network_cidr]
+  dns_servers         = var.virtual_network_dns
+  location            = var.location
+  tags                = {
+    owner       = lookup(var.tag, var.tag.owner, "somebody")
+    email       = lookup(var.tag, var.tag.email, "somebody@mail.com")
+    title       = lookup(var.tag, var.tag.title, "Engineer")
+    department  = lookup(var.tag, var.tag.department, "IS")
+    location    = lookup(var.tag, var.tag.location, "chinanorth")
+    project     = lookup(var.tag, var.tag.project, "Test")
+    environment = lookup(var.tag, var.tag.environment, "SIT")
+  }
 }
