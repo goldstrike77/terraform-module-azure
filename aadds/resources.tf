775e5f2f7f4549952133c9ac2bb1359620375dbc
suzhetao <suzhetao@gmail.com>
2020-11-27 21:16:50 +0800

    Initial commit

--- aadds/resources.tf
+++ aadds/resources.tf
@@ -3,10 +3,11 @@ resource "azurerm_template_deployment" "aadds" {
   name                = "aadds-${title(var.customer)}-${upper(var.env)}-${lower(var.location)}"
   resource_group_name = "rg-${title(var.customer)}-${upper(var.env)}"
   parameters          = {
-    apiVersion              = "2017-06-01"
+    apiVersion              = "2020-01-01"
     name                    = "aadds-${title(var.customer)}-${upper(var.env)}-${lower(var.location)}"
     domainConfigurationType = lookup(var.aadds_spec, "type", "FullySynced")
     domainName              = lookup(var.aadds_spec, "domain", "partner.onmschina.cn")
+    sku                     = lookup(var.aadds_spec, "sku", "Standard")
     filteredSync            = "Disabled"
     location                = var.location
     subnetName              = "snet-${title(var.customer)}-${upper(var.env)}-${lower(var.location)}-Aadds"
@@ -16,7 +17,7 @@ resource "azurerm_template_deployment" "aadds" {
   deployment_mode    = "Incremental"
   template_body      = <<DEPLOY
 {
-  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+  "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
   "contentVersion": "1.0.0.0",
   "parameters": {
     "name": {
@@ -24,6 +25,9 @@ resource "azurerm_template_deployment" "aadds" {
     },
     "apiVersion": {
       "type": "string"
+    },
+    "sku": {
+      "type": "String"
     },
     "domainConfigurationType": {
       "type": "string"
@@ -46,20 +50,38 @@ resource "azurerm_template_deployment" "aadds" {
     "vnetResourceGroup": {
       "type": "string"
     }
-  },  
-    "resources": [{
-        "apiVersion": "[parameters('apiVersion')]",
-        "type": "Microsoft.AAD/DomainServices",
-        "name": "[parameters('name')]",
-        "location": "[parameters('location')]",
-        "properties": {
-          "domainName": "[parameters('domainName')]",
-          "subnetId": "[concat('/subscriptions/', subscription().subscriptionId, '/resourceGroups/', parameters('vnetResourceGroup'), '/providers/Microsoft.Network/virtualNetworks/', parameters('vnetName'), '/subnets/', parameters('subnetName'))]",
-          "filteredSync": "[parameters('filteredSync')]",
-          "domainConfigurationType": "[parameters('domainConfigurationType')]"
-        }
-    }],
-    "outputs": {}
+  },
+  "resources": [
+    {
+      "apiVersion": "[parameters('apiVersion')]",
+      "type": "Microsoft.AAD/DomainServices",
+      "name": "[parameters('name')]",
+      "location": "[parameters('location')]",
+      "properties": {
+        "domainName": "[parameters('domainName')]",
+        "filteredSync": "[parameters('filteredSync')]",
+        "domainConfigurationType": "[parameters('domainConfigurationType')]",
+        "notificationSettings": {
+          "notifyGlobalAdmins": "Enabled",
+          "notifyDcAdmins": "Enabled",
+          "additionalRecipients": []
+        },
+        "domainSecuritySettings": {
+          "ntlmV1": "Enabled",
+          "tlsV1": "Enabled",
+          "syncNtlmPasswords": "Enabled"
+        },
+        "replicaSets": [
+          {
+            "subnetId": "[concat('/subscriptions/', subscription().subscriptionId, '/resourceGroups/', parameters('vnetResourceGroup'), '/providers/Microsoft.Network/virtualNetworks/', parameters('vnetName'), '/subnets/', parameters('subnetName'))]",
+            "location": "[parameters('location')]"
+          }
+        ],
+        "sku": "[parameters('sku')]"
+      }
+    }
+  ],
+  "outputs": {}
 }
 DEPLOY
 }
