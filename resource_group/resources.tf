0d617f92573c446ee934f147f4bf53156d306482
suzhetao <suzhetao@gmail.com>
2021-02-22 10:25:57 +0800

    modified:   resource_group/resources.tf

--- resource_group/resources.tf
+++ resource_group/resources.tf
@@ -1,7 +1,7 @@
 # 创建资源组。
 resource "azurerm_resource_group" "resource_group" {
   count    = length(var.rg_spec)
-  name     = "rg-${title(var.rg_spec[count.index].customer)}-${upper(var.rg_spec[count.index].env)}"
-  location = var.rg_spec[count.index].location
-  tags     = var.rg_spec[count.index].tag
+  name     = var.name
+  location = var.location
+  tags     = var.tag
 }
